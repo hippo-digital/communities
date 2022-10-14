5f31123863934a1ed71d9d3f50cfd3bd2cafcfb9
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-14 21:05:15 +0100

    Move home to index after build

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,34 @@
+const fs = require("node:fs/promises");
 const markdown = require("./lib/markdown.js");
 
 const { GITHUB_REPOSITORY_NAME } = require("./lib/constants.js");
+
+// GitHub Wiki uses /Home as the index, move it to the root.
+async function moveHomeToIndex () {
+    const sourcePath = `_site/Home/index.html`;
+    const targetPath = `_site/index.html`;
+    console.log(`Moving ${sourcePath} to ${targetPath}`)
+    try  {
+        await fs.access(sourcePath);
+        await fs.rename(sourcePath, targetPath);
+        await fs.rmdir("_site/Home/");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary("md", {
     render: (content) => markdown(content),
   });
-  const pathPrefix = GITHUB_REPOSITORY_NAME
-    ? `/${GITHUB_REPOSITORY_NAME}/`
-    : "/";
+
+  // After files are built
+  eleventyConfig.on("eleventy.after", moveHomeToIndex);
+
   return {
     dir: {
       input: "_wiki",
-      pathPrefix,
+      pathPrefix: `/${GITHUB_REPOSITORY_NAME}/`,
     },
   };
 };
