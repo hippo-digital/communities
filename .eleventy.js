be13f9a0bcf93f8a9d816b8e0abbf1b37ebeda87
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-14 19:59:14 +0100

    Fix path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+// Environment variable in GitHub actions deployment.
+const { GITHUB_REPOSITORY } = process.env;
+
 module.exports = function (eleventyConfig) {
+    const pathPrefix = GITHUB_REPOSITORY ? `/${GITHUB_REPOSITORY.split("/")[1]}/` : "/";
   return {
     dir: {
-      input: "_wiki"
+      input: "_wiki",
+      pathPrefix,
     },
   };
 };
