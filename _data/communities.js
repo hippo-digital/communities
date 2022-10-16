9be7edc280766b069fd038ff86779379fe8a3ea7
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-16 15:04:24 +0100

    Improve flags

--- _data/communities.js
+++ _data/communities.js
@@ -3,14 +3,12 @@ module.exports = {
     text: "Disabled Community",
     backgroundColor: "#585858",
     color: "#e9e9e9",
-    image:
-      "https://user-images.githubusercontent.com/2445413/196029122-6f8f0007-fb51-4c55-a923-fb5085e40db0.svg",
+    image: "/assets/communities/disabled-pride-flag.svg",
   },
   "Pride": {
     text: "Pride",
     backgroundColor: "black",
     color: "white",
-    image:
-      "https://user-images.githubusercontent.com/2445413/196036443-2c658ef3-089d-4954-927c-314aeb1114ad.svg",
+    image: "/assets/communities/pride-flag.svg",
   },
 };
