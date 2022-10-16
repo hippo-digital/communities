1e0a57f327d2a8fbe4f96f9f6ec6310de711d940
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-16 15:39:43 +0100

    Fix asset pathes

--- _data/communities.js
+++ _data/communities.js
@@ -3,12 +3,12 @@ module.exports = {
     text: "Disabled Community",
     backgroundColor: "#585858",
     color: "#e9e9e9",
-    image: "/assets/communities/disabled-pride-flag.svg",
+    image: "../assets/communities/disabled-pride-flag.svg",
   },
   "Pride": {
     text: "Pride",
     backgroundColor: "black",
     color: "white",
-    image: "/assets/communities/pride-flag.svg",
+    image: "../assets/communities/pride-flag.svg",
   },
 };
