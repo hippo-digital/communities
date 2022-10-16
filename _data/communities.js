0c51b540d75cb547b4ef89bab916b187ed2a0296
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-16 14:43:22 +0100

    Add better heros for community pages

---
+++ _data/communities.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "Disabled-Community-at-Hippo": {
+    text: "Disabled Community",
+    backgroundColor: "#585858",
+    color: "#e9e9e9",
+    image:
+      "https://user-images.githubusercontent.com/2445413/196029122-6f8f0007-fb51-4c55-a923-fb5085e40db0.svg",
+  },
+  "Pride": {
+    text: "Pride",
+    backgroundColor: "black",
+    color: "white",
+    image:
+      "https://user-images.githubusercontent.com/2445413/196036443-2c658ef3-089d-4954-927c-314aeb1114ad.svg",
+  },
+};
