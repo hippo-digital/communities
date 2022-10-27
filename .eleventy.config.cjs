2a3ebc2fa45733601ce1124b8c8e7e2c59bc0d34
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-27 17:24:35 +0100

    Align with website-from-github-wiki

--- .eleventy.config.cjs
+++ .eleventy.config.cjs
@@ -1,11 +1,27 @@
-const path = require("node:path");
 const {
-  EleventyRenderPlugin,
   EleventyHtmlBasePlugin,
+  EleventyRenderPlugin,
 } = require("@11ty/eleventy");
-const { format } = require("date-fns");
+
+const {
+  DefaultFrontmatterPlugin,
+  RemarkPlugin,
+} = require("./lib/plugins/index.cjs");
+
+const {
+  capitalise,
+  deslug,
+  fileExists,
+  filterBy,
+  formatDate,
+  gitLog,
+  textLength,
+  print,
+} = require("./lib/filters.cjs");
+
 const constants = require("./lib/constants.cjs");
-const { BASE_HREF } = constants;
+const { BASE_HREF, GITHUB_REPOSITORY, GITHUB_REPOSITORY_NAME } = constants;
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
@@ -13,46 +29,58 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
     baseHref: BASE_HREF,
   });
+  eleventyConfig.addPlugin(RemarkPlugin, {
+    plugins: [
+      "./lib/markdown/plugins/external-links.js",
+      "./lib/markdown/plugins/relative-links.js",
+      "./lib/markdown/plugins/responsive-tables.js",
+      "./lib/markdown/plugins/aria-current-links.js",
+      "./lib/markdown/directives/include-partial.js",
+      "remark-breaks",
+      "remark-emoji",
+      "remark-footnotes",
+      "remark-gfm",
+      ["remark-github", { repository: GITHUB_REPOSITORY }],
+      "remark-directive",
+      "remark-slug",
+      "remark-smartypants",
+      [
+        "remark-wiki-link",
+        {
+          pageResolver: (name) => [name.replace(/ /g, "-")],
+          hrefTemplate: (path) => `/${path}`,
+        },
+      ],
+    ],
+  });
 
-  eleventyConfig.setLibrary("md", {
-    disable: () => {}, // TODO: Fix upstream so dont need to do this.
-    render: async (content, pageData) => {
-      {
-        const { default: render } = await import("./lib/markdown/index.js");
-        return render(content, pageData, eleventyConfig);
-      }
+  eleventyConfig.addPlugin(DefaultFrontmatterPlugin, {
+    all: {
+      constants,
+      layout: "page.njk",
+      websiteDescription: "A website generated from a GitHub wiki",
+      websiteTitle: deslug(GITHUB_REPOSITORY_NAME),
+    },
+    "Home.md": {
+      layout: "layout",
+      permalink: "/",
     },
   });
 
+  // Use HTML comments to define frontmatter to keep wiki content cleaner.
   eleventyConfig.setFrontMatterParsingOptions({
     delims: ["<!---", "--->"],
   });
 
-  // Global variables
-  eleventyConfig.addGlobalData("constants", constants);
-  // Set default layout
-  eleventyConfig.addGlobalData("layout", "page.html");
-
   // Filters
-  eleventyConfig.addFilter("slugToTitle", (slug) => {
-    return slug.replace(/-/g, " ");
-  });
-  eleventyConfig.addFilter("textLength", (htmlContent) => {
-    const textContent = htmlContent.replace(/(<([^>]+)>)/gi, "");
-    const trimmedContent = textContent.replace(/\s/g, "").trim();
-    return trimmedContent.length;
-  });
-  eleventyConfig.addFilter("formatDate", (timestamp) => {
-    const date = new Date(timestamp);
-    return `${format(date, "do MMMM yyyy")} at ${format(date, "h:mmaaa")}`;
-  });
-  eleventyConfig.addFilter("getLog", async (inputPath) => {
-    const { default: getLog } = await import("./lib/getLog.js");
-    return await getLog(path.basename(inputPath), eleventyConfig);
-  });
-  eleventyConfig.addFilter("filterBy", (items, propertyName, value) => {
-    return items.find((item) => item[propertyName] === value);
-  });
+  eleventyConfig.addFilter("capitalise", capitalise);
+  eleventyConfig.addFilter("deslug", deslug);
+  eleventyConfig.addFilter("fileExists", fileExists);
+  eleventyConfig.addFilter("formatDate", formatDate);
+  eleventyConfig.addFilter("gitLog", gitLog);
+  eleventyConfig.addFilter("filterBy", filterBy);
+  eleventyConfig.addFilter("textLength", textLength);
+  eleventyConfig.addFilter("print", print);
 
   // Ensure our untracked _wiki input can be used as an input.
   eleventyConfig.setUseGitIgnore(false);
@@ -62,7 +90,6 @@ module.exports = function (eleventyConfig) {
       includes: "../_includes",
       input: "_wiki",
     },
-    templateFormats: ["html", "md"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
   };
